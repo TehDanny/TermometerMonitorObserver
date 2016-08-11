0bdc6ee543f5dc1b7670e235a1ae155196d1eee5
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-08-11 16:38:43 +0200

    Added TemperatureGenerator class and ThreadMethod. Exercise b complete?

---
+++ TermometerMonObs/TermometerMonObs/TemperatureGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermometerMonObs
+{
+    class TemperatureGenerator
+    {
+        public int ThreadMethod()
+        {
+            Random rnd = new Random();
+            int temp = rnd.Next(-20, 120);
+            return temp;
+        }
+    }
+}
