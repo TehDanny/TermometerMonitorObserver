139a0d2248dabcf69b9bd5040d46d1978f9b39b7
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-08-11 14:12:57 +0200

    Excersise a complete

---
+++ TermometerMonObs/TermometerMonObs/Termometer-monitor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermometerMonObs
+{
+    class Termometer_monitor
+    {
+        public int CurrentTemp { get; set; }
+        public int MinTemp { get; set; }
+        public int MaxTemp { get; set; }
+        public int MinAlarm { get; set; }
+        public int MaxAlarm { get; set; }
+        delegate int MinAlarmDelegate(int temp);
+        delegate int MaxAlarmDelegate(int temp);
+
+        public int GetCurrentTemp()
+        {
+            return CurrentTemp;
+        }
+
+        public int GetMinTemp()
+        {
+            return MinTemp;
+        }
+
+        public int GetMaxTemp()
+        {
+            return MaxTemp;
+        }
+
+        public void SetCurrentTemp(int temp)
+        {
+            CurrentTemp = temp;
+
+            if (temp < MinTemp)
+                MinTemp = temp;
+
+            if (temp > MaxTemp)
+                MaxTemp = temp;
+        }
+
+        public void ClearMinAndMaxTemps()
+        {
+            MinTemp = CurrentTemp;
+            MaxTemp = CurrentTemp;
+        }
+    }
+}
